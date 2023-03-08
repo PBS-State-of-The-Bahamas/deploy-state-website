0cac1fa20d43c6fc4dad05e8f4c56ff0b07d544d
Quincy Arthur <quincya@nib-bahamas.com>
2023-03-08 17:03:40 -0500

    remove staging version of cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(sigmabahamas1914.org www.sigmabahamas1914.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="quincyarthur@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
